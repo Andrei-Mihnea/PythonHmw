8c6722a159320e39680a7e3f32c2e4ed3f2750a7
Andrei-Mihnea <andrei.mihnea@student.tuiasi.ro>
2025-07-23 15:16:01 +0300

    Add RequestsLog model to manage request logging in the database

--- Database/db.py
+++ Database/db.py
@@ -9,3 +9,21 @@ DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:123@localhost:5432/m
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
+Base = declarative_base()
+
+
+class RequestsLog(Base):
+    __tablename__ = 'requests_log'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    endpoint = Column(String(255), nullable=False)
+    parameters = Column(Text, nullable=False)
+    result = Column(Text, nullable=False)
+    timestamp = Column(TIMESTAMP, nullable=False)
+
+    def init_db():
+        """Initialize the database."""
+        Base.metadata.create_all(bind=engine)
+
+    def __repr__(self):
+        return f"<RequestsLog(id={self.id}, endpoint={self.endpoint}, parameters={self.parameters}, result={self.result}, timestamp={self.timestamp})>"
