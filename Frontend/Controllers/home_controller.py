ee89647070cc863fba0752c655095e6cb1fdb925
RadicalDev004 <bspilevoi@yahoo.com>
2025-07-29 15:54:09 +0300

    Add JWT authentication to HomeController; update HTML templates for improved user experience and result display

--- Frontend/Controllers/home_controller.py
+++ Frontend/Controllers/home_controller.py
@@ -1,4 +1,5 @@
 # Controllers/home_controller.py
+import jwt
 from Utils.decorators import login_required
 import os
 from flask import request, redirect, make_response
@@ -11,9 +12,16 @@ class HomeController:
         template_path = os.path.join(current_dir, '..', 'Templates', 'home.html')
         template_path = os.path.abspath(template_path)
 
+        token = request.cookies.get("access_token")
+        username = jwt.decode(token, "your-secret-key", algorithms=["HS256"]).get("user_id")
+
+
         with open(template_path, 'r', encoding='utf-8') as f:
             html = f.read()
+
+        html = html.replace("{{username}}", username)
         response = make_response(html)
+
         response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
         response.headers["Pragma"] = "no-cache"
         response.headers["Expires"] = "0"
